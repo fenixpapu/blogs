a9d39ff590d4ac8ae81375f1a136d4dd3bd9e426
Phuc Luong Ngoc (phucln1693) <luong.ngoc.phuc@sun-asterisk.com>
2019-07-03 15:35:09 +0700

    update post 20170702 callback function vs async/await

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-// this refers global object
-function foo() {
-  console.log("Simple function call");
-  console.log(this === window);
-}
+// // this refers global object
+// function foo() {
+//   console.log("Simple function call");
+//   setTimeout(() => console.log('hello'), 2000);
+//   console.log(this === `window`);
+// }
 
-foo(); // prints true on the console
+// foo(); // prints true on the console
+
+
+// #############################  callback vs async/await
+const delay = (ms) => {
+  return new Promise((resolve, reject) => {
+    setTimeout (() => resolve('SUN'), ms);
+  });
+};
+
+const sayHello = (txt) => console.log(`Hello `, txt);
+
+// const testCallBackFunction = (cb) => {
+//   delay(5000)
+//     .then(data => {
+//       cb(data);
+//     });
+//   console.log(`Callback function do other things here!`);
+// }
+
+const testAsyncAwaitFunction = async (cb) => {
+  const data = await delay(5000);
+  cb(data);
+  console.log(`Async/await do other thing here!`)
+}
+testAsyncAwaitFunction(sayHello);
+
